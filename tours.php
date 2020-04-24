35f45025e6768fb9e878c75213e137588d6b9b05
Razaloc <44480264+Razaloc@users.noreply.github.com>
2020-04-24 17:39:15 +0200

    Add files via upload

---
+++ tours.php
@@ -0,0 +1,22 @@
+<?php
+    phpinfo();
+?>
+<!DOCTYPE html>
+
+
+<html lang="es">
+
+</html>
+
+<head>
+	<meta charset="UTF-8">
+	<title>Tours</title>
+</head>
+
+<body>
+	<header>
+		<h1>Tours</h1>
+		<h2>Estos son los tours que realizan nuestros guías, selecciona uno y crea un grupo</h2>
+	</header>
+		
+</body>
